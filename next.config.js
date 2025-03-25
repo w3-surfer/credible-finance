beca67bb49b7d551c89dbae6820bba12af4d00c1
Ravi Aymara <sol@MacBook-Pro-de-Ravi.local>
2025-03-25 01:29:04 -0300

    chore: atualiza dependências e otimiza configuração do Next.js

--- next.config.js
+++ next.config.js
@@ -3,9 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['images.unsplash.com'],
+    unoptimized: true
   },
   experimental: {
     serverActions: true,
+  },
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
   }
 }
 
