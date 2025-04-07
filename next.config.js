1ffcf04ccd291445382dfcf8817b7ebcc9d423ba
Ravi Aymara <seu-email@exemplo.com>
2025-04-07 01:23:50 -0300

    fix: remove critters configuration to fix build error

--- next.config.js
+++ next.config.js
@@ -56,35 +56,6 @@ const nextConfig = {
       },
     };
 
-    // Configuração do Critters
-    if (!dev && !isServer) {
-      const Critters = require('critters');
-      const critters = new Critters({
-        preload: 'swap',
-        preloadFonts: true,
-        fonts: true,
-        noscriptFallback: true,
-      });
-
-      config.plugins.push({
-        apply: (compiler) => {
-          compiler.hooks.compilation.tap('Critters', (compilation) => {
-            compilation.hooks.htmlWebpackPluginAfterHtmlProcessing.tapAsync(
-              'Critters',
-              async (data, cb) => {
-                try {
-                  data.html = await critters.process(data.html);
-                  cb(null, data);
-                } catch (err) {
-                  cb(err);
-                }
-              }
-            );
-          });
-        },
-      });
-    }
-
     return config;
   },
 };
