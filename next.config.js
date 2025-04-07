3a2516daf9dfe6ab8467e7f8f4584836975277ea
Ravi Aymara <seu-email@exemplo.com>
2025-04-07 04:27:42 -0300

    fix: simplify next.config.js to resolve build error

--- next.config.js
+++ next.config.js
@@ -42,31 +42,32 @@ const nextConfig = {
   },
   poweredByHeader: false,
   webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      config.optimization = {
-        ...config.optimization,
-        splitChunks: {
-          chunks: 'all',
-          minSize: 20000,
-          maxSize: 244000,
-          minChunks: 1,
-          maxAsyncRequests: 30,
-          maxInitialRequests: 30,
-          cacheGroups: {
-            defaultVendors: {
-              test: /[\\/]node_modules[\\/]/,
-              priority: -10,
-              reuseExistingChunk: true,
-            },
-            default: {
-              minChunks: 2,
-              priority: -20,
-              reuseExistingChunk: true,
-            },
+    // Otimizações do webpack
+    config.optimization = {
+      ...config.optimization,
+      splitChunks: {
+        chunks: 'all',
+        minSize: 20000,
+        maxSize: 244000,
+        minChunks: 1,
+        maxAsyncRequests: 30,
+        maxInitialRequests: 30,
+        cacheGroups: {
+          defaultVendors: {
+            test: /[\\/]node_modules[\\/]/,
+            priority: -10,
+            reuseExistingChunk: true,
+          },
+          default: {
+            minChunks: 2,
+            priority: -20,
+            reuseExistingChunk: true,
           },
         },
-      };
-    }
+      },
+      runtimeChunk: 'single',
+      moduleIds: 'deterministic',
+    };
     return config;
   },
 }
